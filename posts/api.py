78daaeb191e98b98e5808a4d17c53eeffb8db3da
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-05-16 14:21:19 +0400

    api: add simple API endpoint

--- posts/api.py
+++ posts/api.py
@@ -8,3 +8,8 @@ import decorators
 from posts import app
 from database import session
 
+@app.route("/api/posts", methods=["GET"])
+def posts_get():
+    """ Get a list of posts """
+    data = json.dumps([])
+    return Response(data, 200, mimetype="application/json")
