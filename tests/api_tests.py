2e07e275dd68907d672ae5cdf0b1d7a062612aad
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-05-16 18:27:44 +0400

    tests: add testGetPostsWithTitleAndBody() test
    Does test with query string title_like and body_like. Should return all
    three test posts.

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -142,7 +142,7 @@ class TestAPI(unittest.TestCase):
         session.add_all([postA, postB, postC])
         session.commit()
 
-        response = self.client.get("/api/posts?body_like=bells",
+        response = self.client.get("/api/posts?body_like=Just",
             headers=[("Accept", "application/json")]
         )
 
@@ -150,16 +150,42 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(response.mimetype, "application/json")
 
         posts = json.loads(response.data)
-        self.assertEqual(len(posts), 2)
+        self.assertEqual(len(posts), 1)
 
         post = posts[0]
         self.assertEqual(post["title"], "Post with bells")
         self.assertEqual(post["body"], "Just a test")
 
+    def testGetPostsWithTitleAndBody(self):
+        """ Filtering posts by title and body """
+        postA = models.Post(title="Post with bells", body="Just a test")
+        postB = models.Post(title="Post with whistles", body="Still a test")
+        postC = models.Post(title="Post with bells and whistles",
+                            body="Another test")
+
+        session.add_all([postA, postB, postC])
+        session.commit()
+
+        response = self.client.get(
+            "/api/posts?title_like=Post&body_like=test",
+            headers=[("Accept", "application/json")]
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        posts = json.loads(response.data)
+        self.assertEqual(len(posts), 3)
+
+        post = posts[0]
+        self.assertEqual(post["title"], "Post with bells")
+        self.assertEqual(post["body"], "Just a test")
         post = posts[1]
+        self.assertEqual(post["title"], "Post with whistles")
+        self.assertEqual(post["body"], "Still a test")
+        post = posts[2]
         self.assertEqual(post["title"], "Post with bells and whistles")
         self.assertEqual(post["body"], "Another test") 
-
 
 if __name__ == "__main__":
     unittest.main()
